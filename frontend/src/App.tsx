ffc3e210646de5d22107185b1c2749edd12bba93
Juhwan Oh <bravemeow5@gmail.com>
2023-11-01 00:09:26 -0400

    add userButton function with images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,23 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState} from 'react'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    // Use the fetch function to make a GET request
-    fetch('http://127.0.0.1:5000/data') 
-      .then((data) => {
-        console.log(data); // Update the component's state with the data
-      })
-      .catch((error) => {
-      });
-  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="rps-wrapper">
+        <div className="gpt-move"></div>
+        <div className="user-buttons">
+          <div className="button rock">
+            <img src="/rock.png" alt="" />
+          </div>
+          <div className="button paper">
+            <img src="/paper.png" alt="" />
+          </div>
+          <div className="button scissors">
+            <img src="/scissors.png" alt="" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
