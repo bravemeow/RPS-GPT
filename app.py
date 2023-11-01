ce45de7987980cb533fd6f929f348aa826ea9191
Juhwan Oh <bravemeow5@gmail.com>
2023-10-31 20:42:01 -0400

    connect openai using API

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from openai import sendMessage
+
+app = Flask(__name__)
+
+conversation = [
+    {"role": "system", "content": "Rock Paper Scissors Game God."},
+    {"role": "user", "content": "Let's play Rock Paper Scissors game! Please response only the JSON format without any extra text."}
+]
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.post("/data")
+def sendData():
+    if request.method == 'POST':
+        print(request.form['move'])
+        user_move = request.form['move']
+        user_json = f'{{"move": "{user_move}"}}'
+        user_data = {"role": "user", "content": user_json}
+        conversation.append(user_data)
+        res_text = sendMessage(conversation)
+        system_data = {"role": "assistant", "content": res_text}
+        conversation.append(system_data)
+        print(conversation)
+        return render_template('index.html', res_text=res_text)
+    
+
+app.run(host='localhost', port=5000, debug=True)
---
+++ openai.py
@@ -0,0 +1,16 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+model = "gpt-3.5-turbo"
+def sendMessage(conversation):
+    res = openai.ChatCompletion.create(
+        model=model,
+        messages=conversation
+    )
+    res_text = res.choices[0].message.content
+    print(res)
+    return res_text
